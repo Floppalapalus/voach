0fcab38bf3d2133b4a22053cb94c12afe9aedb42
Floppalapalus <bmorriso18@gmail.com>
2026-05-04 17:50:33 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="voach-v17";
+const CACHE="voach-v18";
 self.addEventListener("install",e=>{self.skipWaiting();});
 self.addEventListener("activate",e=>{self.clients.claim();});
 self.addEventListener("fetch",e=>{if(e.request.url.includes("script.google.com"))return;});
